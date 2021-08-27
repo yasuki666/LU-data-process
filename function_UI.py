7814d6ba277be00ebc815847570aa88d2b1239a1
yasuki666 <1103831994@qq.com>
2021-08-28 01:44:50 +0800

    daily commit

--- function_UI.py
+++ function_UI.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QAction,QMainWindow,QFileDialog,QTextEdit,QMessageBox
+from PyQt5.QtWidgets import QAction, QMainWindow, QFileDialog, QTextEdit, QMessageBox
 from PyQt5.QtWidgets import QApplication
 
 import pyqtgraph as pg
@@ -25,20 +25,21 @@ import pywt
 import wavelet_functions
 import original_UI
 
+
 class Ui_MainWindow(original_UI.Ui_MainWindow):
 
-    def new_set_UI(self,mainWindow):
+    def new_set_UI(self, mainWindow):
         self.setupUi(mainWindow)
 
-        #菜单栏 File
-        #open new file
+        # 菜单栏 File
+        # open new file
         self.actionopen_new_file.triggered.connect(self.open_file)
 
-        #控制区模块 初始化
-        self.column_number_value.setText('1') #默认列数为1
-        self.sampling_rate_value.setText('2500')  #默认采样率为2500MHz
-        self.start_sampling_time_value.setText('1') #默认开始采样时间为1us
-        self.end_sampling_time_value.setText('5') #默认结束采样时间为5us
+        # 控制区模块 初始化
+        self.column_number_value.setText('1')  # 默认列数为1
+        self.sampling_rate_value.setText('2500')  # 默认采样率为2500MHz
+        self.start_sampling_time_value.setText('1')  # 默认开始采样时间为1us
+        self.end_sampling_time_value.setText('5')  # 默认结束采样时间为5us
 
         # 按钮 导入数据
         self.import_data_button.clicked.connect(self.import_data)
@@ -51,19 +52,19 @@ class Ui_MainWindow(original_UI.Ui_MainWindow):
         self.next_column_button.clicked.connect(self.column_number_plus)
 
         # 必须先将四个画布建立好
-        pg.setConfigOption('background', 'w')  #背景色改为白色
-        pg.setConfigOption('foreground', 'k')  #前景色改为黑色
+        pg.setConfigOption('background', 'w')  # 背景色改为白色
+        pg.setConfigOption('foreground', 'k')  # 前景色改为黑色
         self.original_time_canvas = pg.PlotWidget(self.Original_time)  # 创建一个绘图控件
-        self.original_time_canvas.resize(300,160)
+        self.original_time_canvas.resize(300, 160)
         self.original_time_canvas_plot = self.original_time_canvas.plot()
         self.original_frequency_canvas = pg.PlotWidget(self.Original_frequency)
         self.original_frequency_canvas.resize(300, 160)
         self.original_frequency_canvas_plot = self.original_frequency_canvas.plot()
         self.processed_time_canvas = pg.PlotWidget(self.processed_time)
-        self.processed_time_canvas.resize(300,160)
+        self.processed_time_canvas.resize(300, 160)
         self.processed_time_canvas_plot = self.processed_time_canvas.plot()
         self.processed_frequency_canvas = pg.PlotWidget(self.processed_frequency)
-        self.processed_frequency_canvas.resize(300,160)
+        self.processed_frequency_canvas.resize(300, 160)
         self.processed_frequency_canvas_plot = self.processed_frequency_canvas.plot()
 
         # 按钮 作图
@@ -86,18 +87,13 @@ class Ui_MainWindow(original_UI.Ui_MainWindow):
         self.wavelet_type_value.setText('db3')
         self.wavelet_filter_button.clicked.connect(self.wavelet_filter)
 
-
-
-
-
-
-    #函数 打开文件对话窗 将路径保存至文件路径栏
+    # 函数 打开文件对话窗 将路径保存至文件路径栏
     def open_file(self):
-        #注意，这里参数必须传入self.centralwidget，因为QFileDialog只接受widget参数
-        file_name = QFileDialog.getOpenFileName(self.centralwidget,'选择文件','','Excel files(*.xlsx , *.xls , *.csv)')
+        # 注意，这里参数必须传入self.centralwidget，因为QFileDialog只接受widget参数
+        file_name = QFileDialog.getOpenFileName(self.centralwidget, '选择文件', '', 'Excel files(*.xlsx , *.xls , *.csv)')
         self.file_path_value.setText(file_name[0])
 
-    #函数 按文件路径栏已储存的路径将文件数据导入
+    # 函数 按文件路径栏已储存的路径将文件数据导入
     def import_data(self):
         try:
             self.file_data = pd.read_excel(self.file_path_value.text())
@@ -109,11 +105,16 @@ class Ui_MainWindow(original_UI.Ui_MainWindow):
     # 函数 快捷更改文件路径 ex: /totaldata_20210628_50mj.xls' -> /totaldata_20210628_60mj.xls'
     def next_file(self):
         file_path = self.file_path_value.text()
-        num = int(file_path[-8])
+        i = 0
+        while file_path[i] != '.':
+            i -= 1
+
+        num = int(file_path[i - 4])
         num += 1
-        file_path = file_path[:-8] + str(num) + file_path[-7:]
+        file_path = file_path[:i - 4] + str(num) + file_path[i - 3:]
         self.file_path_value.setText(file_path)
-    #函数 数据列数减1
+
+    # 函数 数据列数减1
     def column_number_minus(self):
         num = int(self.column_number_value.text())
         # 判断是否越栈
@@ -121,64 +122,68 @@ class Ui_MainWindow(original_UI.Ui_MainWindow):
             num -= 1
         self.column_number_value.setText(str(num))
 
-    #函数 数据列数加1
+    # 函数 数据列数加1
     def column_number_plus(self):
         num = int(self.column_number_value.text())
         self.column_total_number = self.file_data.shape[1]
         num += 1
-        #判断是否越栈
+        # 判断是否越栈
         if num >= self.column_total_number:
             num -= 1
             QMessageBox.information(self.centralwidget, '提示', '已到达最后一列')
         self.column_number_value.setText(str(num))
 
-    #函数 画图 将原始数据时域图与频域图画出
+    # 函数 画图 将原始数据时域图与频域图画出
     def original_data_time_frequency_drawing(self):
         try:
             # 原始数据时域图
             self.column_num = int(self.column_number_value.text())
-            self.time_aix = self.file_data[:,0]
+            self.time_aix = self.file_data[:, 0]
             self.time_aix = self.time_aix * 1000000
             self.original_time_amplitude_aix = self.file_data[:, self.column_num]
             try:
-                i,j = 0,0
-                while(self.time_aix[i]<int(self.start_sampling_time_value.text())):
+                i, j = 0, 0
+                while (self.time_aix[i] < int(self.start_sampling_time_value.text())):
                     i += 1
-                while(self.time_aix[j]<int(self.end_sampling_time_value.text())):
+                while (self.time_aix[j] < int(self.end_sampling_time_value.text())):
                     j += 1
                 self.time_aix = self.time_aix[i:j]
 
                 self.original_time_amplitude_aix = self.original_time_amplitude_aix[i:j]
-                self.original_time_canvas_plot.setData(self.time_aix, self.original_time_amplitude_aix,pen='b')  # pen参数改变线条颜色，symbol改变点形状，symbolColor改变点颜色
+                self.original_time_canvas_plot.setData(self.time_aix, self.original_time_amplitude_aix,
+                                                       pen='b')  # pen参数改变线条颜色，symbol改变点形状，symbolColor改变点颜色
             except:
                 QMessageBox.information(self.centralwidget, '提示', '采样时间设置有误')
 
             # 原始数据频域图
-            temprory_time_amplitude_aix = self.original_time_amplitude_aix - np.mean(self.original_time_amplitude_aix) #消去直流分量，更能体现频谱信息
-            self.original_frequency_amplitude_aix = np.fft.fft(temprory_time_amplitude_aix) # 快速傅里叶变换
-            self.original_frequency_amplitude_aix = abs(self.original_frequency_amplitude_aix) #转换到实数域
+            temprory_time_amplitude_aix = self.original_time_amplitude_aix - np.mean(
+                self.original_time_amplitude_aix)  # 消去直流分量，更能体现频谱信息
+            self.original_frequency_amplitude_aix = np.fft.fft(temprory_time_amplitude_aix)  # 快速傅里叶变换
+            self.original_frequency_amplitude_aix = abs(self.original_frequency_amplitude_aix)  # 转换到实数域
             n = len(self.original_time_amplitude_aix)
             self.frequency_aix = []
             self.sampling_rate = int(self.sampling_rate_value.text())
-            #得到频域横坐标
+            # 得到频域横坐标
             for i in range(n):
-                self.frequency_aix.append(i * self.sampling_rate/n)
-            self.frequency_aix = self.frequency_aix[:int(n/2)]
-            self.original_frequency_amplitude_aix = self.original_frequency_amplitude_aix[:int(n/2)]
-            #为了方便观看只展示前边0-100MHz部分
-            self.showing_frequency_aix = self.frequency_aix[:int(n*100/(self.sampling_rate))]
-            self.showing_original_frequency_amplitude_aix = self.original_frequency_amplitude_aix[:int(n*100/(self.sampling_rate))]
-            self.original_frequency_canvas_plot.setData(self.showing_frequency_aix, self.showing_original_frequency_amplitude_aix,pen = 'b')
+                self.frequency_aix.append(i * self.sampling_rate / n)
+            self.frequency_aix = self.frequency_aix[:int(n / 2)]
+            self.original_frequency_amplitude_aix = self.original_frequency_amplitude_aix[:int(n / 2)]
+            # 为了方便观看只展示前边0-100MHz部分
+            self.showing_frequency_aix = self.frequency_aix[:int(n * 100 / (self.sampling_rate))]
+            self.showing_original_frequency_amplitude_aix = self.original_frequency_amplitude_aix[
+                                                            :int(n * 100 / (self.sampling_rate))]
+            self.original_frequency_canvas_plot.setData(self.showing_frequency_aix,
+                                                        self.showing_original_frequency_amplitude_aix, pen='b')
         except:
             QMessageBox.information(self.centralwidget, '提示', '未导入数据或数据有误')
 
-    #函数 低通滤波
+    # 函数 低通滤波
     def lowpass_filter(self):
         try:
             self.original_data_time_frequency_drawing()
             frequency_pass = float(self.passbond_boundary_frequency_value.text()) * 1000000
             frequency_stop = float(self.stopband_boundary_frequency_value.text()) * 1000000
-            normalized_frequency_pass = frequency_pass / (self.sampling_rate * 1000000 / 2) #归一化
+            normalized_frequency_pass = frequency_pass / (self.sampling_rate * 1000000 / 2)  # 归一化
             normalized_frequency_stop = frequency_stop / (self.sampling_rate * 1000000 / 2)
             max_passband_loss = 1  # 通带最大衰减 ，默认为1dB
             min_stopband_attenuation = 20  # 阻带最小衰减，默认为20dB
@@ -188,12 +193,13 @@ class Ui_MainWindow(original_UI.Ui_MainWindow):
                                                                          normalized_frequency_stop,
                                                                          max_passband_loss,
                                                                          min_stopband_attenuation)
-            b,a = signal.butter(filter_order,butterworth_natrual_frequency,'lowpass')
-            self.processed_time_amplitude_aix = signal.filtfilt(b,a,self.original_time_amplitude_aix)
-            self.processed_time_canvas_plot.setData(self.time_aix,self.processed_time_amplitude_aix,pen = 'b')
+            b, a = signal.butter(filter_order, butterworth_natrual_frequency, 'lowpass')
+            self.processed_time_amplitude_aix = signal.filtfilt(b, a, self.original_time_amplitude_aix)
+            self.processed_time_canvas_plot.setData(self.time_aix, self.processed_time_amplitude_aix, pen='b')
 
-            #转换至频域
-            temprory_time_amplitude_aix = self.processed_time_amplitude_aix - np.mean(self.processed_time_amplitude_aix)  # 消去直流分量，更能体现频谱信息
+            # 转换至频域
+            temprory_time_amplitude_aix = self.processed_time_amplitude_aix - np.mean(
+                self.processed_time_amplitude_aix)  # 消去直流分量，更能体现频谱信息
             self.processed_frequency_amplitude_aix = np.fft.fft(temprory_time_amplitude_aix)  # 快速傅里叶变换
             self.processed_frequency_amplitude_aix = abs(self.processed_frequency_amplitude_aix)  # 转换到实数域
 
@@ -201,13 +207,33 @@ class Ui_MainWindow(original_UI.Ui_MainWindow):
             self.sampling_rate = int(self.sampling_rate_value.text())
             self.processed_frequency_amplitude_aix = self.processed_frequency_amplitude_aix[:int(n / 2)]
             # 为了方便观看只展示前边0-100MHz部分
-            self.showing_processed_frequency_amplitude_aix = self.processed_frequency_amplitude_aix[:int(n * 100 / (self.sampling_rate))]
+            self.showing_processed_frequency_amplitude_aix = self.processed_frequency_amplitude_aix[
+                                                             :int(n * 100 / (self.sampling_rate))]
             self.processed_frequency_canvas_plot.setData(self.showing_frequency_aix,
                                                          self.showing_processed_frequency_amplitude_aix, pen='b')
+
+            self.original_data_max = max(self.original_time_amplitude_aix)
+            self.original_data_min = min(self.original_time_amplitude_aix)
+            self.original_data_peaktopeak = self.original_data_max - self.original_data_min
+            self.original_data_max = round(self.original_data_max, 6)
+            self.original_data_min = round(self.original_data_min, 6)
+            self.original_data_peaktopeak = round(self.original_data_peaktopeak, 6)
+            self.processed_data_max = max(self.processed_time_amplitude_aix)
+            self.processed_data_min = min(self.processed_time_amplitude_aix)
+            self.processed_data_peaktopeak = self.processed_data_max - self.processed_data_min
+            self.processed_data_max = round(self.processed_data_max, 6)
+            self.processed_data_min = round(self.processed_data_min, 6)
+            self.processed_data_peaktopeak = round(self.processed_data_peaktopeak, 6)
+            self.original_data_maximum_value.setText(str(self.original_data_max))
+            self.original_data_minimum_value.setText(str(self.original_data_min))
+            self.original_data_peaktopeak_value.setText(str(self.original_data_peaktopeak))
+            self.processed_data_maximum_value.setText(str(self.processed_data_max))
+            self.processed_data_minimum_value.setText(str(self.processed_data_min))
+            self.processed_data_peaktopeak_value.setText(str(self.processed_data_peaktopeak))
         except:
             QMessageBox.information(self.centralwidget, '提示', '滤波失败，请检查滤波参数！')
 
-    #函数 带通滤波
+    # 函数 带通滤波
     def bandpass_filter(self):
         try:
             self.original_data_time_frequency_drawing()
@@ -232,23 +258,45 @@ class Ui_MainWindow(original_UI.Ui_MainWindow):
             self.processed_time_amplitude_aix = signal.filtfilt(b, a, self.original_time_amplitude_aix)
             self.processed_time_canvas_plot.setData(self.time_aix, self.processed_time_amplitude_aix, pen='b')
 
-            #变换频域
-            temprory_time_amplitude_aix = self.processed_time_amplitude_aix - np.mean( self.processed_time_amplitude_aix)  # 消去直流分量，更能体现频谱信息
+            # 变换频域
+            temprory_time_amplitude_aix = self.processed_time_amplitude_aix - np.mean(
+                self.processed_time_amplitude_aix)  # 消去直流分量，更能体现频谱信息
             self.processed_frequency_amplitude_aix = np.fft.fft(temprory_time_amplitude_aix)  # 快速傅里叶变换
             self.processed_frequency_amplitude_aix = abs(self.processed_frequency_amplitude_aix)  # 转换到实数域
             n = len(self.processed_time_amplitude_aix)
             self.sampling_rate = int(self.sampling_rate_value.text())
             self.processed_frequency_amplitude_aix = self.processed_frequency_amplitude_aix[:int(n / 2)]
             # 为了方便观看只展示前边0-100MHz部分
-            self.showing_processed_frequency_amplitude_aix = self.processed_frequency_amplitude_aix[:int(n * 100 / (self.sampling_rate))]
+            self.showing_processed_frequency_amplitude_aix = self.processed_frequency_amplitude_aix[
+                                                             :int(n * 100 / (self.sampling_rate))]
             self.processed_frequency_canvas_plot.setData(self.showing_frequency_aix,
-                                                        self.showing_processed_frequency_amplitude_aix, pen='b')
+                                                         self.showing_processed_frequency_amplitude_aix, pen='b')
+
+            self.original_data_max = max(self.original_time_amplitude_aix)
+            self.original_data_min = min(self.original_time_amplitude_aix)
+            self.original_data_peaktopeak = self.original_data_max - self.original_data_min
+            self.original_data_max = round(self.original_data_max, 6)
+            self.original_data_min = round(self.original_data_min, 6)
+            self.original_data_peaktopeak = round(self.original_data_peaktopeak, 6)
+            self.processed_data_max = max(self.processed_time_amplitude_aix)
+            self.processed_data_min = min(self.processed_time_amplitude_aix)
+            self.processed_data_peaktopeak = self.processed_data_max - self.processed_data_min
+            self.processed_data_max = round(self.processed_data_max, 6)
+            self.processed_data_min = round(self.processed_data_min, 6)
+            self.processed_data_peaktopeak = round(self.processed_data_peaktopeak, 6)
+            self.original_data_maximum_value.setText(str(self.original_data_max))
+            self.original_data_minimum_value.setText(str(self.original_data_min))
+            self.original_data_peaktopeak_value.setText(str(self.original_data_peaktopeak))
+            self.processed_data_maximum_value.setText(str(self.processed_data_max))
+            self.processed_data_minimum_value.setText(str(self.processed_data_min))
+            self.processed_data_peaktopeak_value.setText(str(self.processed_data_peaktopeak))
         except:
             QMessageBox.information(self.centralwidget, '提示', '滤波失败，请检查滤波参数！')
 
     # 函数 小波滤波
     def wavelet_filter(self):
         try:
+            self.original_data_time_frequency_drawing()
             # 调用已编写好的wavelet_functions.py的接口
             wavelet_single_demensional_denoising_function_dictionary = {
                 'rigrsure': wavelet_functions.wavelet_single_demensional_denoising_rigrsure,
@@ -291,5 +339,24 @@ class Ui_MainWindow(original_UI.Ui_MainWindow):
                                                              :int(n * 100 / (self.sampling_rate))]
             self.processed_frequency_canvas_plot.setData(self.showing_frequency_aix,
                                                          self.showing_processed_frequency_amplitude_aix, pen='b')
+
+            self.original_data_max = max(self.original_time_amplitude_aix)
+            self.original_data_min = min(self.original_time_amplitude_aix)
+            self.original_data_peaktopeak = self.original_data_max - self.original_data_min
+            self.original_data_max = round(self.original_data_max, 6)
+            self.original_data_min = round(self.original_data_min, 6)
+            self.original_data_peaktopeak = round(self.original_data_peaktopeak, 6)
+            self.processed_data_max = max(self.processed_time_amplitude_aix)
+            self.processed_data_min = min(self.processed_time_amplitude_aix)
+            self.processed_data_peaktopeak = self.processed_data_max - self.processed_data_min
+            self.processed_data_max = round(self.processed_data_max, 6)
+            self.processed_data_min = round(self.processed_data_min, 6)
+            self.processed_data_peaktopeak = round(self.processed_data_peaktopeak, 6)
+            self.original_data_maximum_value.setText(str(self.original_data_max))
+            self.original_data_minimum_value.setText(str(self.original_data_min))
+            self.original_data_peaktopeak_value.setText(str(self.original_data_peaktopeak))
+            self.processed_data_maximum_value.setText(str(self.processed_data_max))
+            self.processed_data_minimum_value.setText(str(self.processed_data_min))
+            self.processed_data_peaktopeak_value.setText(str(self.processed_data_peaktopeak))
         except:
             QMessageBox.information(self.centralwidget, '提示', '滤波失败，请检查滤波参数！')
--- test.py
+++ test.py
@@ -1,7 +1,12 @@
-#path ='C:/Users/11038/Desktop/超声信号处理/totaldata_50mj_20210628_1.xls'#数据路径
+'''#path ='C:/Users/11038/Desktop/超声信号处理/totaldata_50mj_20210628_1.xls'#数据路径
 path ='C:/Users/11038/Desktop/超声信号处理/totaldata_20210628_50mj.xls'#数据路径
 file_path_len = len(path)
 num = int(path[-8])
 num += 1
 path = path[:-8] + str(num) + path[-7:]
-print(path)
+print(path)'''
+
+import numpy as np
+a = np.array([1,2,3,4,5])
+b = max(a)
+print(b)
